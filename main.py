488b3cffae55206e01a788ab3288e60c2d40f74e
Charlie Welsh <charlie@fourange.co>
2017-04-29 01:08:56 +0000

    //WIP Comments II

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Runtime(object):
     def readfile(self, filename):
         #This makes it easier to read the things from the files.
         f = open(filename)
-        #these are the things a room has by default.
+        #these are the things a room has by default. It also sets the variable r = to desc which = None.
         r = desc = None
         items = []
         locks = []
@@ -98,45 +98,70 @@ class Runtime(object):
                         locks = []
                         exits = {}
                     r = value
+        #This sets what the Runtime sees from the file as a room.
         if r is not None:
+            #This prints "adding last room [room number] at [roomlist]"
             print("adding last room", r, "at", len(self.roomlist))
+            #This has the variable "rm" which had been previously defined, equal to a room with the parameters from the
+            # file.
             rm = Room(r, exits, desc, items, locks)
+            #this sets the room in the roomlist equal to the room previously defined in "rm". It just reuses it and
+            # replaces whatever was previously there.
             self.roomlist[r] = rm
+        #this closes the file.
         f.close()
 
+    #This is the run function. This is the actual game portion.
     def run(self):
+        #this runs the clear screen command in a system prompt, clearing the screen for the game to look nice.
         os.system('cls')
+        #while True is just a way to do an infinite loop.
         while True:
+            #prints the room you are in.
             print("room:", self.room)
+            #prints the description of the room description.
             print("You are in", self.roomlist[self.room].desc)
+            #prints the items in the room
             print("items here:", self.roomlist[self.room].items)
+            #prints the items in the player's inventory
             print("you are carrying:", self.inventory)
+            #if there are no locks for that room, it displays the locked doors, and what exits you can exit into.
             if self.roomlist[self.room].locks is not None:
                 print("the locked doors are:", self.roomlist[self.room].locks)
             print("You can go", self.roomlist[self.room].exits)
+            #this is what the input is. Anything after the ">" is considered input, and is equal to "i".
             i = input(">").lower()
+            #this is a variable that keeps track of whether or not a command is valid..
             cancel = 0
+            #if the input is "unlock", "use key" or "u", while there is a key in the player's inventory, consume the key
+            #  and unlock the door.
             if (i == 'unlock' or i == 'use key' or i == 'u') and 'key' in self.inventory:
                 print('The door unlocks with a resounding clunk.')
                 self.inventory.remove('key')
                 self.roomlist[self.room].locks = []
+            #this just prints what's in that print statement down there.
             elif (i == 'unlock' or i == 'use key' or i == 'u'):
                 print('There is no key in your inventory or there are no exits with locks in this room.')
                 cancel = 1
             r = None
-            if i == "t" or i == 'take':
+            #if t, take, or take all are inputted,it removes the items from the room and adds them to the player's
+            # inventory.
+            if i == "t" or i == 'take' or i == 'take all':
                 self.inventory.extend(self.roomlist[self.room].items)
                 self.roomlist[self.room].items = []
+            #if a direction is typed in the prompt, move to the room in that direction.
             elif i in ['n', 'north', 's', 'south', 'e', 'east', 'w', 'west']:
                 dirname = i[0]
+                #if the door is locked, don't let the player move to that room, and report that the door is locked.
                 if dirname in self.roomlist[self.room].exits:
                     if dirname in self.roomlist[self.room].locks:
                         print('That door is locked. Type U to unlock the door if you have a key.')
                     else:
                         r = self.roomlist[self.room].exits[dirname]
+                #if a user types in a direction that is incorrect, it notifies the user as such.
                 else:
                     print('Invalid exit. Retry your command.')
-
+            #if an invalid command is typed, it notifies the user as such.
             elif cancel == 0:
                 print("That is invalid. Retype your command.")
                 continue
